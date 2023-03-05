319e5859136ea3aa249fbf27e5225d948ef73839
Aashka Doshi <aashkadoshi7@gmail.com>
2023-03-06 00:16:56 +0530

    home and about

---
+++ src/components/home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import About from '../about'
+import Hero from '../hero'
+
+const HomeComponent = () => (
+  <>
+    <Hero />
+    <About />
+  </>
+)
+
+export default HomeComponent
---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      xsm: "320px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    colors: {
+      "primary-500": "#91A117",
+      "secondary-500": "#EDD11D",
+      "black-400": '#363031',
+      "black-500": "#231F20",
+      "white-500": "#FBFBFF",
+    },
+    extend: {
+      fontFamily: {
+        logo: ["Rubik Iso"],
+        title: ["VT323"],
+        body: ["Source Code Pro"],
+      },
+    },
+    plugins: [],
+  }
+}
